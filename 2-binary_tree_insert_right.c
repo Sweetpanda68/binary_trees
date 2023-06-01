e8be183b6f0b95d800629a2a05c9f8caa45d04a1
Dawit Tadesse <dawittadesse927@gmail.com>
2023-06-01 21:43:11 +0300

    fix: minor

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -17,11 +17,11 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	if (parent == NULL)
+	if (!parent)
 		return (NULL);
 
 	new = binary_tree_node(parent, value);
-	if (new == NULL)
+	if (!new)
 		return (NULL);
 
 	if (parent->right != NULL)

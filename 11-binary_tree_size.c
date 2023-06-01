ef7771c9e10dd7b7720f5073c02b310abbebbf45
Dawit Tadesse <dawittadesse927@gmail.com>
2023-06-01 23:59:39 +0300

    Refactor

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -10,11 +10,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t size = 0;
 
-	if (tree)
-	{
-		size += 1;
-		size += binary_tree_size(tree->left);
-		size += binary_tree_size(tree->right);
-	}
-	return (size);
+	if (!tree)
+		return (0);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
